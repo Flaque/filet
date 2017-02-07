121f0375a5a22fcda982b9dc59d8d5d1bae610ff
Flaqueeau <econrad@zagmail.gonzaga.edu>
2017-02-07 08:50:07 -0800

    Switch to testify instead of Goblin
    Goblin requires way to much boilerplate code.

--- filet_test.go
+++ filet_test.go
@@ -1,78 +1,63 @@
 package filet
 
 import (
-	. "github.com/franela/goblin"
 	"testing"
   "path/filepath"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestTmpDir(t *testing.T) {
   defer CleanUp(t)
 
-  g := Goblin(t)
-  g.Describe("TmpDir", func() {
-    g.It("should create the directory", func() {
-      path := TmpDir(t, "")
-      g.Assert(Exists(t, path)).Equal(true)
-    })
-  })
+	path := TmpDir(t, "")
+	assert.Equal(t, Exists(t, path), true,
+		"TmpDir should create a directory")
 }
 
 func TestTmpFile(t *testing.T) {
   defer CleanUp(t)
 
-  g := Goblin(t)
-  g.Describe("TmpFile", func() {
-    g.It("should create the file", func() {
-      file := TmpFile(t, "", "")
-      g.Assert(Exists(t, file.Name())).Equal(true)
-    })
+	// Test that file is actually created
+	file := TmpFile(t, "", "")
+	assert.Equal(t, Exists(t, file.Name()), true,
+		"TmpFile should create the file")
 
-    g.It("should create the file", func() {
-      file := TmpFile(t, "", "hey there")
-      g.Assert(FileSays(t, file.Name(), []byte("hey there"))).Equal(true)
-    })
-  })
+	// Test that the content exists in the file
+	file = TmpFile(t, "", "hey there")
+	result := FileSays(t, file.Name(), []byte("hey there"))
+	assert.Equal(t, result, true,
+		"TmpFile should create a file with content")
 }
 
 func TestFileSays(t *testing.T) {
   defer CleanUp(t)
 
-  g := Goblin(t)
-  g.Describe("FileSays", func() {
-    g.It("should correctly identify strings from files", func() {
-      file := TmpFile(t, "", "Ghandi")
-      g.Assert(FileSays(t, file.Name(), []byte("Ghandi"))).Equal(true)
-      g.Assert(FileSays(t, file.Name(), []byte("nope!"))).Equal(false)
-    })
-  })
+	file := TmpFile(t, "", "Ghandi")
+	assert.Equal(t, FileSays(t, file.Name(), []byte("Ghandi")), true,
+		"FileSays can correctly read a file.")
+	assert.Equal(t, FileSays(t, file.Name(), []byte("nope!")), false,
+		"FileSays can correctly tell when a file does not contain content.")
 }
 
 func TestExists(t *testing.T) {
   defer CleanUp(t)
 
-  g := Goblin(t)
-  g.Describe("Exists", func() {
-    g.It("should correctly tell when something exists", func() {
-      file := TmpFile(t, "", "I exist")
-      g.Assert(Exists(t, file.Name())).Equal(true)
-      g.Assert(Exists(t, "BlahBlahBlahNopeNope")).Equal(false)
-    })
-  })
+	file := TmpFile(t, "", "I exist")
+	assert.Equal(t, Exists(t, file.Name()), true,
+		"Exists should correctly tell if a path exists")
+	assert.Equal(t, Exists(t, "blahblahblah"), false,
+		"Exists should correctly tell if a path does not exist")
 }
 
 func TestDirContains(t *testing.T) {
   defer CleanUp(t)
 
-  g := Goblin(t)
-  g.Describe("DirContains", func() {
-    g.It("should correctly tell when a path is inside another dir", func() {
-      dir := TmpDir(t, "")
-      filePath := filepath.Base(TmpFile(t, dir, "").Name())
-      g.Assert(DirContains(t, dir, filePath)).Equal(true)
-      g.Assert(DirContains(t, dir, "bloobloobloo")).Equal(false)
-    })
-  })
+	dir := TmpDir(t, "")
+	filePath := filepath.Base(TmpFile(t, dir, "").Name())
+	assert.Equal(t, DirContains(t, dir, filePath), true,
+		"DirContains should correctly identify a file inside of a folder.")
+	assert.Equal(t, DirContains(t, dir, "blahlleijiow"), false,
+		"DirContains should correctly identify a file not inside of a folder.")
 }
 
 func TestCleanUp(t *testing.T) {
@@ -81,20 +66,17 @@ func TestCleanUp(t *testing.T) {
   // Clear testRegistry
   testRegistry = make([]string, 0)
 
-  g := Goblin(t)
-  g.Describe("CleanUp", func() {
-    g.It("should correctly tell when a path is cleaned up", func() {
-      dir := TmpDir(t, "")
-      secondDir := TmpDir(t, "")
+	// Create test files
+	dir := TmpDir(t, "")
+	secondDir := TmpDir(t, "")
 
-      // Clean up and test that the files are gone
-      CleanUp(t)
-      g.Assert(Exists(t, dir)).Equal(false)
-      g.Assert(Exists(t, secondDir)).Equal(false)
+	// Clean up and test that the files are gone
+	CleanUp(t)
+	assert.Equal(t, Exists(t, dir), false, "CleanUp should remove files.")
+	assert.Equal(t, Exists(t, secondDir), false, "CleanUp should remove files.")
 
-      // Create a new file and test that it's there.
-      newDir := TmpDir(t, "")
-      g.Assert(Exists(t, newDir)).Equal(true)
-    })
-  })
+	// Create a new file and test that it's there.
+	newDir := TmpDir(t, "")
+	assert.Equal(t, Exists(t, newDir), true,
+		"New files still exist after CleanUp")
 }
