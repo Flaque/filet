151a2ce80b6053078be566981bdd43b344dd5672
Flaqueeau <econrad@zagmail.gonzaga.edu>
2017-02-07 19:21:20 -0800

    gofmt

--- filet.go
+++ filet.go
@@ -1,10 +1,10 @@
 package filet
 
 import (
-  "github.com/spf13/afero"
-  "testing"
-  "bytes"
-  "path/filepath"
+	"bytes"
+	"github.com/spf13/afero"
+	"path/filepath"
+	"testing"
 )
 
 // Keeps track of files that we've used so we can clean up.
@@ -15,40 +15,40 @@ var appFs afero.Fs = afero.NewOsFs()
 Creates a tmp directory for us to use.
 */
 func TmpDir(t *testing.T, dir string) string {
-  name, err := afero.TempDir(appFs, dir, "dir")
-  if err != nil {
-  t.Error("Failed to create the tmpDir: " + name, err)
-  }
+	name, err := afero.TempDir(appFs, dir, "dir")
+	if err != nil {
+		t.Error("Failed to create the tmpDir: "+name, err)
+	}
 
-  Files = append(Files, name)
-  return name
+	Files = append(Files, name)
+	return name
 }
 
 /*
 Creates a tmp file for us to use when testing
 */
 func TmpFile(t *testing.T, dir string, content string) afero.File {
-  file, err := afero.TempFile(appFs, dir, "file")
-  if err != nil {
-  t.Error("Failed to create the tmpFile: " + file.Name(), err)
-  }
+	file, err := afero.TempFile(appFs, dir, "file")
+	if err != nil {
+		t.Error("Failed to create the tmpFile: "+file.Name(), err)
+	}
 
-  file.WriteString(content)
-  Files = append(Files, file.Name())
+	file.WriteString(content)
+	Files = append(Files, file.Name())
 
-  return file
+	return file
 }
 
 /*
 Returns true if the file at the path contains the expected byte array content.
- */
+*/
 func FileSays(t *testing.T, path string, expected []byte) bool {
-  content, err := afero.ReadFile(appFs, path)
-  if err != nil{
-    t.Error("Failed to read file: " + path, err)
-  }
+	content, err := afero.ReadFile(appFs, path)
+	if err != nil {
+		t.Error("Failed to read file: "+path, err)
+	}
 
-  return bytes.Equal(content, expected)
+	return bytes.Equal(content, expected)
 }
 
 /*
@@ -56,32 +56,32 @@ Removes all files in our test registry and calls `t.Error` if something goes
 wrong.
 */
 func CleanUp(t *testing.T) {
-  for _, path := range Files {
-  if err := appFs.RemoveAll(path); err != nil {
-  t.Error(appFs.Name(), err)
-  }
-  }
+	for _, path := range Files {
+		if err := appFs.RemoveAll(path); err != nil {
+			t.Error(appFs.Name(), err)
+		}
+	}
 
-  Files = make([]string, 0)
+	Files = make([]string, 0)
 }
 
 /*
 Returns true if the file exists. Calls t.Error if something goes wrong while
 checking.
- */
+*/
 func Exists(t *testing.T, path string) bool {
-  exists, err := afero.Exists(appFs, path)
-  if err != nil {
-  t.Error("Something went wrong when checking if " + path + "exists!", err)
-  }
-  return exists
+	exists, err := afero.Exists(appFs, path)
+	if err != nil {
+		t.Error("Something went wrong when checking if "+path+"exists!", err)
+	}
+	return exists
 }
 
 /*
 Returns true if the dir contains the path. Calls t.Error if something goes wrong while
 checking.
- */
+*/
 func DirContains(t *testing.T, dir string, path string) bool {
-  fullPath := filepath.Join(dir, path)
-  return Exists(t, fullPath)
+	fullPath := filepath.Join(dir, path)
+	return Exists(t, fullPath)
 }
